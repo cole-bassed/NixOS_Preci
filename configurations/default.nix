75866ade18f3d83273a83564a5faba45f068bc36
cole-bassed <75517056+cole-bassed@users.noreply.github.com>
2026-06-05 12:55:26 -0500

    modules

--- configurations/default.nix
+++ configurations/default.nix
@@ -10,23 +10,18 @@
 #       inherit (flake) inputs;
 #       inherit (libraries) lib lix;
 #     };
-
-  # modules = importModules (moduleArgs // {base = ./modules;});
-
-  # secrets = importModules (moduleArgs // {base = ../secrets;});
-
-  # groupImports = groups: concatMap (group: group.imports or []) groups;
-  # groupHome = groups: concatMap (group: group.home-manager.sharedModules or []) groups;
-{libraries,...}@base: let
-
-in
-  libraries.assemble.configurations base {
+# modules = importModules (moduleArgs // {base = ./modules;});
+# secrets = importModules (moduleArgs // {base = ../secrets;});
+# groupImports = groups: concatMap (group: group.imports or []) groups;
+# groupHome = groups: concatMap (group: group.home-manager.sharedModules or []) groups;
+{libraries, ...} @ base: libraries.assemble.configurations base {
+    modules = ./modules/base;
     # modules.core = [
     #   ({host, ...}: {
     #     system.stateVersion = host.stateVersion or null;
     #     # config.system.stateVersion = config.system.nixos.release;
     #   })
-    # ] 
+    # ]
     # ++ groupImports (modules.imports or []) ++ (secrets.imports or []);
 
     # modules.home =
---
+++ configurations/modules/default.nix
@@ -0,0 +1,24 @@
+# flake: let
+#   inherit (flake.libraries.modules) importModules;
+
+#   core =
+#     (flake.modules.core or [])
+#     ++ [
+#       (importModules {
+#         base = ./.;
+#         args = flake;
+#         excludes =
+#           flake.defaults.excludes
+#           ++ [
+#             "ai"
+#             "applications"
+#             "interface"
+#           ];
+#       })
+#     ];
+# in
+#   flake.libraries.mkConfigurations {
+#     class = "nixos";
+#     inherit flake;
+#     # flake = flake // {modules = flake.modules // {inherit core;};};
+#   }
