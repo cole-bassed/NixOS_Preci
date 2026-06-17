0856ee31c09e0c03b2d248110b71c701fa14edd6
Craole <32288735+Craole@users.noreply.github.com>
2026-06-17 08:26:26 -0500

    moved config

--- libraries/default.nix
+++ libraries/default.nix
@@ -10,19 +10,74 @@
         (paths.bootstrap or ./base))
   ) {inherit paths;};
 
-  external = import (
-    paths.store.libraries.external or
-      (paths.libraries.external or
-        (paths.external or ./external))
-  ) {inherit bootstrap defaults flake names paths;};
+  inherit (bootstrap.attrsets) merge;
+  inherit (bootstrap.filesystem) mkPaths;
+  inherit (bootstrap.config) mkLibrary;
 
-  internal =
-    import (
-      paths.store.libraries.internal or
-      (paths.libraries.internal or
-        (paths.internal or ./internal))
-    ) {
-      inherit bootstrap external;
-      inherit (external) flake defaults names paths;
+  external = mkLibrary {
+    base = ./external;
+    excludes = ["default"];
+    seed = {
+      inherit bootstrap defaults flake names paths;
     };
-in {inherit bootstrap external internal;}
+    enableAliases = false;
+    enableExtras = false;
+  };
+
+  internal = mkLibrary {
+    base = ./internal;
+    excludes = ["default" "base" "bootstrap"];
+    seed =
+      merge
+      (merge bootstrap external)
+      {
+        inherit bootstrap external;
+        flake = external.flake or {};
+        defaults = merge defaults (
+          merge {
+            host = "ExampleHost";
+            excludes.paths = [
+              "archive"
+              "backup"
+              "review"
+              "temp"
+              "default.nix"
+              "flake.nix"
+            ];
+            tags = ["core" "home"];
+          } (external.flake.defaults or {})
+        );
+
+        paths = mkPaths {
+          paths = merge paths (
+            merge {
+              store = {
+                src = ../.;
+                api = ../configuration/api;
+              };
+              local.src = "/etc/nixos";
+            } (external.flake.paths or {})
+          );
+        };
+
+        names = merge names (
+          merge {
+            src = "dots";
+            lib = "lix";
+            top = "_";
+          } (external.flake.names or {})
+        );
+      };
+    extra = merge external bootstrap;
+    enableAliases = false;
+    enableExtras = false;
+  };
+
+  config = mkLibrary {
+    base = ./config;
+    excludes = ["default"];
+    seed = internal;
+    enableAliases = true;
+    enableExtras = false;
+  };
+in {inherit bootstrap config external internal;}
